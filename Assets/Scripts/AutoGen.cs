e462aba05c52c62ad1b74fc0aca32caaef2b0dfd
Pradeep Giri <pradeepgiri.pg123@gmail.com>
2021-03-03 09:22:07 +0530

    Added Corountine function

--- Assets/Scripts/AutoGen.cs
+++ Assets/Scripts/AutoGen.cs
@@ -8,18 +8,60 @@ public class AutoGen : MonoBehaviour
     private GameObject s;
     [SerializeField]
     private Transform pos;
+
+    private int number = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("WaitForSphare");
+        //Invoke("SphareInstantiate", 5f);
+        //InvokeRepeating("SphareInstantiate", 5f, 1f);
+        //Time.timeScale = 0;
+        InvokeRepeating("CountDown", 2f, 1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CountDown()
     {
-        if(Input.GetMouseButtonDown(0))
+        number--;
+    }
+
+    bool NoCount()
+    {
+        Debug.Log("Number - " + number);
+        
+        if(number <= 0)
         {
-            Instantiate(s, pos);
+            CancelInvoke("CountDown");
+            return true;
+        }
+        else
+        {
+            return false;
+        }  
+    }
+
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        StopCoroutine("WaitForSphare");
+    //        Debug.Log("Stop Coroutine");
+    //    }
+    //}
+
+    void SphareInstantiate()
+    {
+        Instantiate(s, pos);
+    }
+
+    IEnumerator WaitForSphare()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(NoCount);
+            SphareInstantiate();
+            yield return new WaitForSeconds(2);
+            Debug.Log("Function called after 2 second");
         }
     }
 }
---
+++ Assets/Scripts/CorutineDemo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorutineDemo : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(WaitForSomeTime());
+    }
+
+    IEnumerator WaitForSomeTime()
+    {
+        yield return new WaitForSeconds(10f);
+        // Debug.Log("Function called after 10 second");
+    }
+}
